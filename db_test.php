9ca3038a183ab626e9e2097ae53ab287c5699a13
yoshiaki <himajin315@gmail.com>
2013-09-23 04:23:30 +0900

    myqli-conection

--- db_test.php
+++ db_test.php
@@ -8,34 +8,34 @@
 <?php
 print('<p>始まり</p>');
 
-$db = mysqli_init(); 
+$mysqli = mysqli_init(); 
 
 print('<p>初期化</p>');
 
-$db->ssl_set('key/himajin315-key.pem','key/himajin315-cert.pem',
+$mysqli->ssl_set('key/himajin315-key.pem','key/himajin315-cert.pem',
 'key/cleardb-ca.pem',NULL,NULL); 
 print('<p>キー取得完了</p>');
 
-$db->real_connect('ap-cdbr-azure-east-b.cloudapp.net', 'himajin315', '7a9e3c9f', 'sample');
+$mysqli->real_connect('ap-cdbr-azure-east-b.cloudapp.net', 'befd75233a2af5', '7a9e3c9f' ,'ramen');
 print('<p>データベース接続完了</p>');
 
-
+echo 'Success... ' . $mysqli->host_info . "\n";
 
 mysql_set_charset('utf8');
 
-$db = mysql_query('SELECT id,name FROM test');
-if (!$result) {
-    die('クエリーが失敗しました。'.mysql_error());
+if ($result = $mysqli->query("SELECT name FROM test LIMIT 10;")) {
+    printf("Select returned %d rows.\n", $result->num_rows);
+    while ($row = $result->fetch_row()) {
+         printf("%s\n", $row[0]);
+    }
+
+    /* 結果セットを開放します */
+    $result->close();
 }
 
-while ($row = mysql_fetch_assoc($result)) {
-    print('<p>');
-    print('id='.$row['id']);
-    print(',name='.$row['name']);
-    print('</p>');
-}
 
-$db->close();
+
+$mysqli->close();
 print('<p>データベース接続解除</p>');
 
 ?>
